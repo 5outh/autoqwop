e3808f3518fdbcddd61f945e91ca3ef50464abc9
5outh <bkovach13@gmail.com>
2014-11-16 17:34:43 -0500

    Extract metres

--- src/quickGrab.py
+++ src/quickGrab.py
@@ -1,16 +1,29 @@
 import ImageGrab
+import Image
 import os
 import time
  
 start_x, start_y = 520, 270
 end_x, end_y = 1159, 668
 
+metres_start_x, metres_start_y = 170, 24
+metres_end_x, metres_end_y = 413, 46
+
 def screenGrab():
     box = (start_x, start_y, end_x, end_y)
     im = ImageGrab.grab(box)
-    im.save(os.getcwd() + '\\qwop__' + str(int(time.time())) +
-'.png', 'PNG')
- 
+    impath = os.getcwd() + '\\qwop\\qwop__' + str(int(time.time())) + '.png' 
+    im.save(impath, 'PNG')
+
+    box = (metres_start_x, metres_start_y, metres_end_x, metres_end_y)
+
+    metres = im.crop(box)
+    metres_path = impath[:-4] + '_metres.png'
+
+    print(metres_path)
+
+    metres.save(metres_path, 'PNG')
+
 def main():
     screenGrab()
  
