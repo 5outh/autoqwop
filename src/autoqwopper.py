a541b20a0944acee99622797a29f1941b4a08427
5outh <bkovach13@gmail.com>
2014-11-16 18:28:44 -0500

    start and finish game

--- src/autoqwopper.py
+++ src/autoqwopper.py
@@ -10,12 +10,18 @@ import win32api, win32con
 start_x, start_y = 520, 270
 end_x, end_y = 1159, 668
 
+frame = (start_x, start_y, end_x, end_y)
+
 # Bounding box for the "metres" dialogue box
 metres_start_x, metres_start_y = 170, 24
 metres_end_x, metres_end_y = 413, 46
 
+metres_box = (metres_start_x, metres_start_y, metres_end_x, metres_end_y)
+
 # x, y coordinate of the ribbon that pops up when you die
 ribbon_x, ribbon_y = 155, 125
+
+ribbon_pixel = (ribbon_x, ribbon_y)
 
 VK_CODE = {
     'SPACE':0x20,
@@ -25,26 +31,10 @@ VK_CODE = {
     'W':0x57
     }
 
-# TODO: Implement
-class AutoQwopper:
-    pass
-
-def screenGrab():
-    box = (start_x, start_y, end_x, end_y)
-    im = ImageGrab.grab(box)
-    impath = os.getcwd() + '\\qwop\\qwop__' + str(int(time.time())) + '.png' 
-    im.save(impath, 'PNG')
-
-    isDead(im);
-
-    box = (metres_start_x, metres_start_y, metres_end_x, metres_end_y)
-
-    metres = im.crop(box)
-    metres_path = impath[:-4] + '_metres.png'
-
-    metres.save(metres_path, 'PNG')
-
-    return im
+def sendKey(key, duration=0.1):
+    win32api.keybd_event(key, 0, 0, 0)
+    time.sleep(duration)
+    win32api.keybd_event(key, 0, win32con.KEYEVENTF_KEYUP, 0)
 
 def leftClick(coords, duration=0.1):
     win32api.SetCursorPos((start_x + coords[0], start_y + coords[1]))
@@ -55,33 +45,34 @@ def leftClick(coords, duration=0.1):
     
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
-def sendKey(key, duration=0.1):
-    win32api.keybd_event(key, 0, 0, 0)
-    time.sleep(duration)
-    win32api.keybd_event(key, 0, win32con.KEYEVENTF_KEYUP, 0)
+# TODO: Implement
+class AutoQwopper:   
+    def __init__(self):
+        self.update()
+    
+    def update(self):
+        self.qwop_frame = ImageGrab.grab(frame)
+        self.metres_frame = self.qwop_frame.crop(metres_box)
 
-def getCoords():
-    x,y = win32api.GetCursorPos()
-    x = x - start_x
-    y = y - start_y
-    print x,y
+    def isDead(self):
+        return (self.qwop_frame.getpixel(ribbon_pixel) == (255, 255, 0))
 
-def beginGame():
-    leftClick((10, 10))
+    def beginGame(self):
+        leftClick((10, 10))
 
-def restartGame():
-    sendKey(VK_CODE['SPACE'])
+    def restartGame(self):
+        sendKey(VK_CODE['SPACE'])
 
-def isDead(image):
-    return (image.getpixel((ribbon_x, ribbon_y)) == (255, 255, 0))
+    def run(self):
+        self.beginGame()
+        while (not self.isDead()):
+            time.sleep(0.5)
+            self.update()
+        print "DEAD"
 
 def main():
-    beginGame()
-    im = screenGrab()
-    while (not isDead(im)):
-        time.sleep(0.5)
-        im = screenGrab()
-    restartGame()
+    qwopper = AutoQwopper()
+    qwopper.run()
  
 if __name__ == '__main__':
     main()
