3e348e84d28d44e1149242cd536d4d3f504f8b50
luke-grantham <lgrantham2@gmail.com>
2014-12-09 22:06:04 -0500

    rawr

--- src/autoqwopper.py
+++ src/autoqwopper.py
@@ -219,6 +219,51 @@ def mutate(ind):
     ind[randint(0, len(ind)-1)] = chr(randint(65, 80))
     return ind
 
+def orderedx(list1, list2):
+    result = [0 for x in range(len(list1))]
+    
+    point1 = random.randint(0, 4)
+    point2 = random.randint(0, 4)
+            
+    if point2 < point1:
+        temp = point1
+        point1 = point2
+        point2 = temp
+    
+    if(point2 == 4 and point1 == 0):
+        #prevents child being the same as parent
+        point2 = random.randint(0,3)
+
+    #Order Crossover Step1: copy randomly selected
+    #segment from first parent into offspring
+    for x in range(point1, point2+1):
+        result[x] = list1[x]
+        
+    list2newOrder = []
+    for x in list2[point2+1:]:
+        list2newOrder.append(x)
+    for x in list2[:point2+1]:
+        list2newOrder.append(x)
+    
+    #Order Crossover steop2: copy rest of alleles
+    #in order they appear in second parent
+    for x in list2newOrder:
+        continueflag = 0
+        for y in range(point2+1, len(list2)):
+            if result[y] == 0:
+                result[y] = x
+                continueflag = 1
+                break
+        if(continueflag == 1):
+            continueflag = 0
+            continue
+        for y in range(point2+1):
+            if result[y] == 0:
+                result[y] = x
+                break
+    
+    return result
+    
 toolbox = base.Toolbox()
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness = creator.FitnessMax)
@@ -267,7 +312,7 @@ def main():
         parent1 = toolbox.clone(selected[0])
         parent2 = toolbox.clone(selected[1])
         
-        child = toolbox.orderedCross(parent1, parent2)[0] #crossover
+        child = toolbox.orderedx(parent1, parent2) #crossover
         child = mutate(child)
         
         child.fitness.values = evaluate(child) #evaluate child
